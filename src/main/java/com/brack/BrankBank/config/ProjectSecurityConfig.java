cce5e9c89036016468c65ec66d7fff6879987ade
Alex Brackenridge <a.brack@live.co.uk>
2023-06-16 18:07:20 +0100

    Security config

---
+++ src/main/java/com/brack/BrankBank/config/ProjectSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.brack.BrankBank.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
+    }
+
+}
