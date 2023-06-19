4381a889b8ec58fe0d32d6ea022c16351de89d68
Alex Brackenridge <a.brack@live.co.uk>
2023-06-19 17:56:56 +0100

    Using MySQL for user storage

--- src/main/java/com/brack/BrankBank/config/ProjectSecurityConfig.java
+++ src/main/java/com/brack/BrankBank/config/ProjectSecurityConfig.java
@@ -7,26 +7,29 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
 //CUSTOM SECURITY CONFIGURATIONS
         http.authorizeHttpRequests()
-                        .requestMatchers("/my-account", "/my-balance", "/my-cards").authenticated()
-                        .requestMatchers("/notices", "/contact").permitAll()
+                .requestMatchers("/my-account", "/my-balance", "/my-cards").authenticated()
+                .requestMatchers("/notices", "/contact").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
-
 
 
 //DENY ALL TEST
@@ -35,7 +38,6 @@ public class ProjectSecurityConfig {
 //                .and().formLogin()
 //                .and().httpBasic();
 //        return http.build();
-
 
 
 // PERMIT ALL REQUESTS TEST
@@ -49,40 +51,47 @@ public class ProjectSecurityConfig {
 
 
     //NOT RECOMMENDED FOR PROD
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//
+//        //Approach 1
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("admin")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("user")
+//                .authorities("user")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//
+//        //Approach 2
+//
+////            UserDetails admin = User.withUsername("admin").password("admin").authorities("admin").build();
+////
+////            UserDetails user = User.withUsername("user").password("user").authorities("user").build();
+////
+////            return new InMemoryUserDetailsManager(admin, user);
+//        }
+//
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        //Approach 1
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin")
-                .authorities("admin")
-                .build();
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .authorities("user")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-
-        //Approach 2
-
-//            UserDetails admin = User.withUsername("admin").password("admin").authorities("admin").build();
-//
-//            UserDetails user = User.withUsername("user").password("user").authorities("user").build();
-//
-//            return new InMemoryUserDetailsManager(admin, user);
-        }
-
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return NoOpPasswordEncoder.getInstance();
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
 
 
 
