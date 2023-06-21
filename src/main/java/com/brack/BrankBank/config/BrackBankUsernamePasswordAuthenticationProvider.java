80a1e17176319d6d5aa1d5fb4de4eff3e3e9c034
Alex Brackenridge <a.brack@live.co.uk>
2023-06-21 13:42:35 +0100

    Customer authentication provider

---
+++ src/main/java/com/brack/BrankBank/config/BrackBankUsernamePasswordAuthenticationProvider.java
@@ -0,0 +1,51 @@
+package com.brack.BrankBank.config;
+
+
+import com.brack.BrankBank.model.Users;
+import com.brack.BrankBank.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class BrackBankUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        List<Users> users = userRepository.findByEmail(username);
+        if(users.size() > 0){
+            if(passwordEncoder.matches(password, users.get(0).getPassword())){
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(users.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, password, authorities);
+            } else {
+                throw new BadCredentialsException("Incorrect credentials");
+            }
+        } else {
+            throw new BadCredentialsException("No user found");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
